87447e412cf4e0e26a77a59fb59ba03cf74cb08b
d1212 <d1212>
2016-09-28 11:39:01 +0900

    課題

---
+++ vector.rb
@@ -0,0 +1,17 @@
+require 'dxruby'
+
+class Vector
+
+	def initialize(x, y)
+		@x=x
+		@y=y
+    end
+
+	def to_s
+		return "(#{@x},#{@y})"
+	end
+
+	def length
+		return Math.sqrt(@x**2 + @y**2)
+	end
+end
